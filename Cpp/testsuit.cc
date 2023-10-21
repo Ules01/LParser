b0bf7edc8431561f27941f7d1866d667833b58f4
Jules LEROQUAIS <jules.leroquais@epita.fr>
2023-10-21 13:08:31 +0200

    add test semi advanced part 1

--- Cpp/testsuit.cc
+++ Cpp/testsuit.cc
@@ -42,9 +42,78 @@ int basic(){
     return launchTestSuit(testSuit);
 }
 
+int semi_advanced(void){
+    struct testStruct testSuit = initTestSuit("Semi advanced");
+
+
+    LParser lParser = LParser("a + bc");
+    testSuit = addTest(lParser.isaccept("a"), false, "Semi advanced or and: a and [a + bc]", testSuit);
+    testSuit = addTest(lParser.isaccept("b"), false, "Semi advanced or and: b and [a + bc]", testSuit);
+    testSuit = addTest(lParser.isaccept("c"), false, "Semi advanced or and: c and [a + bc]", testSuit);
+    testSuit = addTest(lParser.isaccept("ac"), true, "Semi advanced or and: ac and [a + bc]", testSuit);
+    testSuit = addTest(lParser.isaccept("bc"), true, "Semi advanced or and: bc and [a + bc]", testSuit);
+    testSuit = addTest(lParser.isaccept("ca"), false, "Semi advanced or and: ca and [a + bc]", testSuit);
+
+
+    lParser = LParser("(ab) + c");
+    testSuit = addTest(lParser.isaccept("ac"), false, "Semi advanced (and) or: ac in [(ab) + c]", testSuit);
+    testSuit = addTest(lParser.isaccept("abc"), false, "Semi advanced (and) or: abc in [(ab) + c]", testSuit);
+    testSuit = addTest(lParser.isaccept("ab"), true, "Semi advanced (and) or: ab in [(ab) + c]", testSuit);
+    testSuit = addTest(lParser.isaccept("c"), true, "Semi advanced (and) or: c in [(ab) + c]", testSuit);
+
+    lParser = LParser("ab*");
+    testSuit = addTest(lParser.isaccept("a"), true, "Semi advanced and *: a in [ab*]", testSuit);
+    testSuit = addTest(lParser.isaccept("ab"), true, "Semi advanced and *: ab in [ab*]", testSuit);
+    testSuit = addTest(lParser.isaccept("abbbb"), true, "Semi advanced and *: abbbb in [ab*]", testSuit);
+    testSuit = addTest(lParser.isaccept("b"), false, "Semi advanced and *: b in [ab*]", testSuit);
+    testSuit = addTest(lParser.isaccept(""), false, "Semi advanced and *: $ in [ab*]", testSuit);
+
+    lParser = LParser("(ab)*");
+    testSuit = addTest(lParser.isaccept("a"), false, "Semi advanced and *: a in [(ab)*]", testSuit);
+    testSuit = addTest(lParser.isaccept("aba"), false, "Semi advanced and *: aba in [(ab)*]", testSuit);
+    testSuit = addTest(lParser.isaccept(""), true, "Semi advanced and *: $ in [(ab)*]", testSuit);
+    testSuit = addTest(lParser.isaccept("ab"), true, "Semi advanced and *: ab in [(ab)*]", testSuit);
+    testSuit = addTest(lParser.isaccept("ababab"), true, "Semi advanced and *: ababab in [(ab)*]", testSuit);
+
+    lParser = LParser("(a + b)*");
+    testSuit = addTest(lParser.isaccept("a"), true, "Semi advanced (or)*: a in [(a + b)*]", testSuit);
+    testSuit = addTest(lParser.isaccept("abababbbabba"), true, "Semi advanced (or)*: abababbbabba in [(a + b)*]", testSuit);
+    testSuit = addTest(lParser.isaccept("bbbbbbb"), true, "Semi advanced (or)*: bbbbbbb in [(a + b)*]", testSuit);
+    testSuit = addTest(lParser.isaccept(""), true, "Semi advanced (or)*: $ in [(a + b)*]", testSuit);
+    testSuit = addTest(lParser.isaccept("c"), false, "Semi advanced (or)*: c in [(a + b)*]", testSuit);
+
+    lParser = LParser("a + b*");
+    testSuit = addTest(lParser.isaccept("a"), true, "Semi advanced or *: a in [a + b*]", testSuit);
+    testSuit = addTest(lParser.isaccept(""), true, "Semi advanced or *: $ in [a + b*]", testSuit);
+    testSuit = addTest(lParser.isaccept("b"), true, "Semi advanced or *: b in [a + b*]", testSuit);
+    testSuit = addTest(lParser.isaccept("bbbb"), true, "Semi advanced or *: bbb in [a + b*]", testSuit);
+    testSuit = addTest(lParser.isaccept("ab"), false, "Semi advanced or *: ab in [a + b*]", testSuit);
+    testSuit = addTest(lParser.isaccept("ba"), false, "Semi advanced or *: ba in [a + b*]", testSuit);
+
+
+    lParser = LParser("(a + b)!");
+    testSuit = addTest(lParser.isaccept("a"), true, "Semi advanced (or)!: a in [(a + b)!]", testSuit);
+    testSuit = addTest(lParser.isaccept("abababbbabba"), true, "Semi advanced (or)!: abababbbabba in [(a + b)!]", testSuit);
+    testSuit = addTest(lParser.isaccept("bbbbbbb"), true, "Semi advanced (or)!: bbbbbbb in [(a + b)!]", testSuit);
+    testSuit = addTest(lParser.isaccept(""), false, "Semi advanced (or)!: $ in [(a + b)!]", testSuit);
+    testSuit = addTest(lParser.isaccept("c"), false, "Semi advanced (or)!: c in [(a + b)!]", testSuit);
+
+    lParser = LParser("a + b!");
+    testSuit = addTest(lParser.isaccept("a"), true, "Semi advanced or !: a in [a + b!]", testSuit);
+    testSuit = addTest(lParser.isaccept(""), false, "Semi advanced or !: $ in [a + b!]", testSuit);
+    testSuit = addTest(lParser.isaccept("b"), true, "Semi advanced or !: b in [a + b!]", testSuit);
+    testSuit = addTest(lParser.isaccept("bbbb"), true, "Semi advanced or !: bbb in [a + b!]", testSuit);
+    testSuit = addTest(lParser.isaccept("ab"), false, "Semi advanced or !: ab in [a + b!]", testSuit);
+    testSuit = addTest(lParser.isaccept("ba"), false, "Semi advanced or !: ba in [a + b!]", testSuit);
+
+    
+
+    return launchTestSuit(testSuit);
+}
+
 int main(void) {
     int res = basic();
-
+    res = res && semi_advanced();
     if (res)
         return 0;
     return -1;
--- draw.sh
+++ draw.sh
@@ -4,12 +4,9 @@ if [ $# -ne 0 ]; then
   cd Cpp/src
   cmake CMakeLists.txt
   make
-  echo $1
-  ./draw $1
 
   rm graphivz/graph.dot
   ./draw $1 >> graphivz/graph.dot
-
   rm -rf CMakeFiles cmake_install.cmake Makefile draw CMakeCache.txt
 
   rm graphivz/graph.dot.png
